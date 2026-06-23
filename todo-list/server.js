5625d35b8d6ff722c9bd3e12502c9dd94a8a95dd
Egor <egorhlystov239@gmail.com>
2026-06-23 23:27:11 +0300

    5homwrk

--- todo-list/server.js
+++ todo-list/server.js
@@ -1,26 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const methodOverride = require('method-override'); 
-const taskRoutes = require('./routes/task.routes'); 
+const methodOverride = require('method-override');
+const taskRoutes = require('./routes/task.routes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
-// Мидлвары
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
-app.use(methodOverride('_method')); 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.use('/', taskRoutes);
 
-const MONGO_URI = 'mongodb://localhost:27017/todo_db';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/todo_db';
 
 mongoose.connect(MONGO_URI)
     .then(() => console.log('Успешное подключение к MongoDB'))
     .catch(err => console.error('Ошибка подключения к MongoDB:', err));
 
 app.listen(PORT, () => {
-    console.log(`Сервер запущен на http://localhost:${PORT}`);
+    console.log(`Сервер запущен на порту ${PORT}`);
 });
+
+module.exports = app;
