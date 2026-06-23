6ffb47a74e5ba9a09e4f7e2f9c69fd8e204f4295
Egor <egorhlystov239@gmail.com>
2026-06-23 23:14:17 +0300

    4homew

--- todo-list/controllers/task.controller.js
+++ todo-list/controllers/task.controller.js
@@ -1,9 +1,10 @@
 const Todo = require('../todo.model');
+const { validationResult } = require('express-validator');
 
 exports.getAllTasks = async (req, res) => {
     try {
         const tasks = await Todo.find().sort({ createdAt: -1 });
-        res.status(200).render('index', { todos: tasks });
+        res.status(200).render('index', { todos: tasks, errors: null });
     } catch (error) {
         res.status(500).send('Ошибка при получении списка задач: ' + error.message);
     }
@@ -11,6 +12,15 @@ exports.getAllTasks = async (req, res) => {
 
 exports.createTask = async (req, res) => {
     try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const tasks = await Todo.find().sort({ createdAt: -1 });
+            return res.status(400).render('index', { 
+                todos: tasks, 
+                errors: errors.array() 
+            });
+        }
+
         const { title } = req.body;
         const newTask = new Todo({ title });
         await newTask.save();
@@ -24,20 +34,17 @@ exports.updateTask = async (req, res) => {
     try {
         const { id } = req.params;
         const { completed } = req.body;
-
         const updatedTask = await Todo.findByIdAndUpdate(
             id, 
             { completed: completed === 'true' }, 
             { new: true, runValidators: true }
         );
-
         if (!updatedTask) {
             return res.status(404).send('Задача с таким ID не найдена');
         }
-
         res.redirect('/');
     } catch (error) {
-        res.status(400).send('Ошибка при обновлении задачи: ' + error.message);
+        res.status(400).send('Ошибка при Побновлении задачи: ' + error.message);
     }
 };
 
@@ -45,12 +52,10 @@ exports.deleteTask = async (req, res) => {
     try {
         const { id } = req.params;
         const deletedTask = await Todo.findByIdAndDelete(id);
-
         if (!deletedTask) {
             return res.status(404).send('Задача с таким ID не найдена');
         }
-
-        res.redirect('/'); 
+        res.redirect('/');
     } catch (error) {
         res.status(500).send('Ошибка при удалении задачи: ' + error.message);
     }
--- todo-list/routes/task.routes.js
+++ todo-list/routes/task.routes.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const { body } = require('express-validator');
 const taskController = require('../controllers/task.controller');
 
 router.get('/', taskController.getAllTasks);
 
-router.post('/todos', taskController.createTask);
+router.post('/todos', [
+    body('title')
+        .trim()
+        .notEmpty().withMessage('Название задачи не может быть пустым')
+        .isLength({ min: 3 }).withMessage('Название должно содержать минимум 3 символа')
+], taskController.createTask);
+
 router.put('/todos/:id', taskController.updateTask);
 router.delete('/todos/:id', taskController.deleteTask);
 
